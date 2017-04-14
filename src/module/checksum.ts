ed3bc7d3e6e7e9f54037fb5ff9b136f8bc618155
haniyama_wataru <haniyama_wataru@sgk.jp>
2017-04-14 14:50:02 +0900

    add checksum module

--- src/module/checksum.ts
+++ src/module/checksum.ts
@@ -1,25 +1,55 @@
-import * as Crypto from 'crypto-js';
+import * as crypto from 'crypto';
+import * as fs from 'fs';
+import { Promise } from 'es6-promise';
 
+/**Class for calculate checksum */
 export class Sumchecker {
-    private type: ChecksumType;
+    private _type: ChecksumType;
     constructor(type: ChecksumType) {
-        this.type = type;
+        this._type = type;
     }
-    private createChecker(type: ChecksumType): Crypto.Hash {
+    private createChecker(type: ChecksumType): crypto.Hash {
         switch (type) {
             case ChecksumType.MD5:
-                return Crypto.MD5;
+                return crypto.createHash(ChecksumType.MD5.toLowerCase());
             case ChecksumType.SHA1:
-                return Crypto.SHA1;
+                return crypto.createHash(ChecksumType.SHA1.toLowerCase());
             case ChecksumType.SHA256:
-                return Crypto.SHA256;
+                return crypto.createHash(ChecksumType.SHA256.toLowerCase());
             default:
                 throw new Error();
         }
     }
-    public check(input: string): string {
-        const checker = this.createChecker(this.type);
-        return checker(input);
+    /**
+     * return checksum for Promise
+     * You can use like below
+     *  check(filename).then((checksum) => {use checksum})
+     * @param input filepath or ReadStream of file
+     */
+    public checkfile(input: fs.ReadStream | string): Promise<string> {
+        let stream:fs.ReadStream;
+        if(typeof(input) == 'string'){
+            stream = fs.createReadStream(input);
+        }else{
+            stream = input;
+        }
+        return new Promise<string>((resolve: (value: string) => void, reject) => {
+            const sum = this.createChecker(this._type);
+            try {
+                stream.on('data', (data: string) => sum.update(data));
+                stream.on('close', () => {
+                    resolve(sum.digest('hex'));
+                })
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    public checkString(str:string){
+        const sum = this.createChecker(this._type);        
+        sum.update(str);
+        return sum.digest();
     }
 }
 
